da60c57b9ec3f47d794ddf027a653980673afe13
kunchapavankalyan <pavankalyankuncha@gmail.com>
2025-08-23 20:16:20 +0530

    "added few more testcases for the coverage"

--- src/test/java/com/TODO/TODO/Service/Task_ServiceTest.java
+++ src/test/java/com/TODO/TODO/Service/Task_ServiceTest.java
@@ -2,6 +2,7 @@ package com.TODO.TODO.Service;
 
 import com.TODO.TODO.Entity.Task;
 import com.TODO.TODO.Entity.Todo_input;
+import com.TODO.TODO.Exception.ResourseNotFoundException;
 import com.TODO.TODO.Repository.Todo_Repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class Task_ServiceTest {
+class TaskServiceTest {
 
     @Mock
     private Todo_Repository todoRepository;
@@ -63,4 +64,60 @@ class Task_ServiceTest {
         assertEquals("Sample", result.get().getTitle());
         verify(todoRepository, times(1)).findById(1L);
     }
+
+    @Test
+    void testUpdateTask()
+    {
+        Task existingTask=new Task();
+        existingTask.setId(1L);
+        existingTask.setTitle("work1");
+
+        Task UpdatingTask=new Task();
+        UpdatingTask.setTitle("work2");
+
+        when(todoRepository.findById(1L)).thenReturn(Optional.of(existingTask));
+        when(todoRepository.save(any(Task.class))).thenReturn(existingTask);
+
+        Task updated=taskService.update_task(1L,UpdatingTask);
+
+        assertEquals(1L,updated.getId());
+        assertEquals("work2",updated.getTitle());
+        assertNotEquals("work1",updated.getTitle());
+        assertTrue(updated.isCompleted());
+    }
+
+    @Test
+    void testDeleteTask()
+    {
+        Task existingTask=new Task();
+        existingTask.setId(1L);
+        existingTask.setTitle("apple");
+        existingTask.setDescription("an apple a keeps a doctor away");
+        existingTask.setCompleted(false);
+
+
+        when(todoRepository.findById(1L)).thenReturn(Optional.of(existingTask));
+        doNothing().when(todoRepository).deleteById(1L);
+
+        taskService.delete(existingTask.getId());
+
+        verify(todoRepository, times(1)).deleteById(1L);
+        verify(todoRepository,never()).findById(1L);
+
+    }
+    @Test
+    void testDeleteTask_NotFound() {
+        Long id = 1L;
+
+        // Arrange: no task found
+        when(todoRepository.findById(id)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(ResourseNotFoundException.class, () -> taskService.delete(id));
+
+        // Verify interactions
+        verify(todoRepository, times(1)).findById(id);
+        verify(todoRepository, never()).deleteById(anyLong());
+    }
+
 }
